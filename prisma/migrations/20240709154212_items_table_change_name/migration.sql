1c7f1d951ceaba5cc76968a7714a9e53ba4d7f66
Johaner <providencesatterfield@gmail.com>
2024-07-10 00:46:10 +0900

    change name of table for items with "items" and add router for metadata

---
+++ prisma/migrations/20240709154212_items_table_change_name/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - You are about to drop the `item` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `item`;
+
+-- CreateTable
+CREATE TABLE `items` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `description` VARCHAR(191) NULL,
+    `image` VARCHAR(191) NULL,
+    `price` DOUBLE NULL,
+    `properties` VARCHAR(191) NULL,
+    `size` INTEGER NULL,
+    `blockchain` VARCHAR(191) NULL,
+    `royatity` DOUBLE NULL,
+    `createdAt` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NULL,
+    `deletedAt` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const apiRouter = require("./api");
 const actionsRouter = require("./actions");
+const metadataRouter = require("./actions");
 
 router.use("/api", apiRouter);
 router.use("/actions", actionsRouter);
+router.use("/actions", metadataRouter);
 
 module.exports = router;
