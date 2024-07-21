fa9fd2021446a014b40aebc9183b6ceaf987ac7f
Johaner <providencesatterfield@gmail.com>
2024-07-21 09:20:41 +0900

    cancel offer api

--- routes/api/offers.js
+++ routes/api/offers.js
@@ -17,42 +17,75 @@ const prisma = new PrismaClient();
 
 router.post("/", async (req, res) => {
   try {
-    const errors = validateOffer(req.body);
-    if (errors.length > 0) {
-      return responseError(res, errors[0]);
-    }
-    let user = await prisma.users.findFirst({
-      where: {
-        wallet_address: req.body.from_wallet_address,
-      },
-    });
-    if (!user) {
-      user = await prisma.users.create({
-        data: {
+    if (!req.body.id) {
+      const errors = validateOffer(req.body);
+      if (errors.length > 0) {
+        return responseError(res, errors[0]);
+      }
+      let user = await prisma.users.findFirst({
+        where: {
           wallet_address: req.body.from_wallet_address,
         },
       });
-      debug("New User", user);
+      if (!user) {
+        user = await prisma.users.create({
+          data: {
+            wallet_address: req.body.from_wallet_address,
+          },
+        });
+        debug("New User", user);
+      }
+      const offer = await prisma.offers.findFirst({
+        where: {
+          user_id: user.id,
+          item_id: req.body.item_id,
+          status: "new",
+        },
+      });
+      if (offer) {
+        return responseError(res, t("errors.already_made_offer"));
+      }
+      await prisma.offers.create({
+        data: {
+          user_id: user.id,
+          item_id: req.body.item_id,
+          price: req.body.price,
+          status: "new",
+        },
+      });
+      return responseData(res);
+    } else {
+      // get offer
+      const offer = await prisma.offers.findUnique({
+        where: {
+          id: req.body.id,
+        },
+      });
+      if (!offer) {
+        return responseError(
+          res,
+          t("errors.validation.invalid", { name: "offer" })
+        );
+      }
+
+      const fields = ["status"];
+      const values = {};
+      fields.forEach((field) => {
+        if (req.body[field]) {
+          values[field] = req.body[field];
+        }
+      });
+
+      // update offer
+      await prisma.offers.update({
+        where: {
+          id: req.body.id,
+        },
+        data: values,
+      });
+
+      return responseData(res);
     }
-    const offer = await prisma.offers.findFirst({
-      where: {
-        user_id: user.id,
-        item_id: req.body.item_id,
-        status: "new",
-      },
-    });
-    if (offer) {
-      return responseError(res, t("errors.already_made_offer"));
-    }
-    await prisma.offers.create({
-      data: {
-        user_id: user.id,
-        item_id: req.body.item_id,
-        price: req.body.price,
-        status: "new",
-      },
-    });
-    return responseData(res, {});
   } catch (error) {
     debug(error);
     return responseError(res, t("errors.internal_error"));
